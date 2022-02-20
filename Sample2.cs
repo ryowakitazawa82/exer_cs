c9ddf394447d95765e5f93c8e334b834671154ac
ryowa.kitazawa <88272934+ryowakitazawa82@users.noreply.github.com>
2022-02-20 22:56:46 +0900

    fisrt commit

---
+++ Sample2.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sample2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string name ="よっちゃん"; //宣言と代入を同時に
+        Debug.Log(name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ test.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+//ファイル名とクラス名は同じにする（小文字と大文字は区別されるので注意）
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int x = 100;
+        Debug.Log(x);
+
+        string name ="よっちゃん"; //宣言と代入を同時に
+        Debug.Log(name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
